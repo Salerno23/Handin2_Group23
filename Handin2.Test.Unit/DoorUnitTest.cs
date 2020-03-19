53aa98b16d86ac66f9171e98c57c45419edffddd
Stine F. Gravesen <au553805@post.au.dk>
2020-03-19 21:45:15 +0100

    Added DoorUnitTest class and first test

---
+++ Handin2.Test.Unit/DoorUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ladeskab;
+using NUnit.Framework;
+
+namespace Handin2.Test.Unit
+{
+    public class DoorUnitTest
+    {
+        private Door _uut;
+        private DoorStateChangedEventArgs _receivedEventArgs;
+
+        [SetUp]
+        public void Setup()
+        {
+            _receivedEventArgs = null;
+
+            _uut = new Door();
+
+            //eventlistener
+            _uut.DoorStateChangedEvent += (o, args) =>
+            {
+                _receivedEventArgs = args;
+            };
+        }
+
+        [Test]
+        public void setDoorStateToNewState_TestEventFired()
+        {
+            _uut.SetDoorState(true);
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+            
+        }
+
+    }
+}
