894579182815f39664244e65c37386a287d60d00
Rene Street <20112809@post.au.dk>
2020-03-19 18:30:04 +0100

    Added RFID folder, and RFID implementation

---
+++ Handin2/RFID/ReadRFIDEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handin2.RFID
+{
+    public class ReadRFIDEventArgs : EventArgs
+    {
+        public int RFIDTag { get; set; }
+    }
+}
