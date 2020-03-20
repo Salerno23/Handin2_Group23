d35be76df224d09be37bdc51183edd80afd99c89
Stine F. Gravesen <au553805@post.au.dk>
2020-03-20 17:37:59 +0100

    Installed NSubstitute. Created StationControlUnitTest

---
+++ Handin2.Test.Unit/StationControlUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Handin2.RFID;
+using Ladeskab;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Handin2.Test.Unit
+{
+    [TestFixture]
+    public class StationControlUnitTest
+    {
+        private StationControl _uut;
+        private IDoor _door;
+        private IRFIDReader _rfidReader;
+
+        [SetUp]
+        public void Setup()
+        {
+            _door = Substitute.For<IDoor>();
+            _rfidReader = Substitute.For<IRFIDReader>();
+
+            _uut = new StationControl(_door, _rfidReader);
+
+            
+        }
+    }
+}
